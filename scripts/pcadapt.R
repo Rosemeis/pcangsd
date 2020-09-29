d5a8e17a725f9d0409a774f753cdba47e7a42532
Jonas <jonas.meisner@bio.ku.dk>
2020-09-29 18:03:50 +0200

    Create pcadapt.R

---
+++ scripts/pcadapt.R
@@ -0,0 +1,25 @@
+####################
+# Run pcadapt scan #
+####################
+#
+#	args[1]: zscores matrix
+#	args[2]: prefix for output files
+#
+
+args = commandArgs(trailingOnly=TRUE)
+
+library(RcppCNPy)
+library(bigutilsr)
+
+zscores <- npyLoad(args[1])
+K <- ncol(zscores)
+
+# For one component only
+if (K == 1) {
+	d2 <- (zscores - median(zscores))^2
+} else {
+	d2 <- dist_ogk(zscores)
+}
+
+write.table(d2, file=paste0(args[2], ".pcadapt.test.txt"), quote=F, row.names=F, col.names=F)
+write.table(pchisq(d2, df=K, lower.tail=F), file=paste0(args[2], ".pcadapt.pval.txt"), quote=F, row.names=F, col.names=F)
